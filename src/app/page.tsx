29ddf522d264b6ca362af6b6bbb13460cec0f336
alexis anzieu <alexis.anzieu@ssense.com>
2023-06-06 21:14:13 -0400

    try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Products from "./products";
 import useSWR from "swr";
 
 // tslint:disable-next-line
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
 
 function useProducts() {
     const { data, error, isLoading } = useSWR(
